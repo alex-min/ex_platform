91efdd304895c06198ab75cc546f6b19f440ed76
Alex-min <alexandre.minette@pennylane.tech>
2021-12-12 20:07:18 +0100

    webpack isn't used anymore

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,3 @@
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
 //
 // Import deps with the dep name or local files with a relative path, for example:
 //
--- config/dev.exs
+++ config/dev.exs
@@ -16,8 +16,7 @@ config :ex_platform, ExPlatform.Repo,
 # debugging and code reloading.
 #
 # The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with webpack to recompile .js and .css sources.
+# watchers to your application. For example, we use it to recompile .js and .css sources.
 config :ex_platform, ExPlatformWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
